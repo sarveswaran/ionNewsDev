78f670512a65ffb84957b77d2b37deb1b91919aa
bharath <bharath.d@mantralabsglobal.com>
2017-01-26 20:28:26 +0530

    new css

---
+++ Modules/Authentication/Database/Migrations/2017_01_25_200501_update_usertable.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateUsertable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+
+            $table->string('phone');
+            $table->string('address');
+            $table->string('role');
+            $table->boolean('status')->default(0);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('', function (Blueprint $table) {
+
+        });
+    }
+}
