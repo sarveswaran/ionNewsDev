829d4f70daca7051e8a4e4963529f2b2128bcebd
Naval Kishore G R <bharathd363@gmail.com>
2017-01-28 10:20:41 +0530

    likes user structure change

--- Modules/Questions/Database/Migrations/2017_01_27_075910339612_create_questions_likes_table.php
+++ Modules/Questions/Database/Migrations/2017_01_27_075910339612_create_questions_likes_table.php
@@ -19,7 +19,8 @@ class CreateQuestionsLikesTable extends Migration
             $table->integer('question_id')->unsigned();
             $table->foreign('question_id')->references('id')->on('questions__questions')->onDelete('cascade');
             $table->integer('count');
-            $table->json('users');
+            $table->integer('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
