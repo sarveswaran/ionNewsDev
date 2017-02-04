2f854ccfb63c1970af3e7b7c7876ebef41cf4b7d
bharath <bharath.d@mantralabsglobal.com>
2017-02-04 12:39:43 +0530

    blocks

--- Modules/Questions/Http/Controllers/Api/QuestionsApiController.php
+++ Modules/Questions/Http/Controllers/Api/QuestionsApiController.php
@@ -78,7 +78,7 @@ class QuestionsApiController extends Controller
     {
         $all = $this->questions->findByAttributes(['category_id' => $category]);
         if(isset($all->id)){
-            $questions = $this->questions->findByAttributes(['category_id' => $category])->paginate();
+            $questions = $this->questions->findByAttributes([])->paginate();
             return Response::json($questions);
         }else{
              return Response::json(['message' => 'no data']);
