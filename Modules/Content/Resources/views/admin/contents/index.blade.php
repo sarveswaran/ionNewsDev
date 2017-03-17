583a1540f78ef3163d522142a3704e291acd398a
bharath <bharath.d@mantralabsglobal.com>
2017-03-17 10:41:00 +0530

    content srawl

--- Modules/Content/Resources/views/admin/contents/index.blade.php
+++ Modules/Content/Resources/views/admin/contents/index.blade.php
@@ -14,11 +14,11 @@
     <div class="row">
         <div class="col-xs-12">
             <div class="row">
-                 <div class="btn-group pull-right" style="margin: 0 15px 15px 0;">
+                <!--  <div class="btn-group pull-right" style="margin: 0 15px 15px 0;">
                     <a href="{{ route('admin.crawl.crawlcontent.index') }}" class="btn btn-primary btn-flat" style="padding: 4px 10px;">
                         <i class="fa fa-pencil"></i> {{ trans('Crawl content') }}
                     </a>
-                </div>
+                </div> -->
                 <div class="btn-group pull-right" style="margin: 0 15px 15px 0;">
                     <a href="{{ route('admin.content.content.create') }}" class="btn btn-primary btn-flat" style="padding: 4px 10px;">
                         <i class="fa fa-pencil"></i> {{ trans('content::contents.button.create content') }}
