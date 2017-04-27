44f0aefa8eb094122caa4c3c1d72928ea44ab8be
pradeep0492 <pradeep.gupta@mantralabsglobal.com>
2017-04-27 12:50:41 +0530

    Add API for Like Story

--- Modules/Content/Http/Controllers/Api/StoryController.php
+++ Modules/Content/Http/Controllers/Api/StoryController.php
@@ -18,11 +18,14 @@ use Modules\Services\Repositories\UsertypeRepository;
 use Modules\Authentication\Events\Confirmnotify;
 use Modules\Content\Repositories\ContentRepository;
 use Modules\Content\Repositories\CategoryRepository;
+use Modules\Content\Repositories\ContentLikeStoryRepository;
+use Modules\Content\Entities\ContentLikeStory;
 use Log;
+use DB;
 class StoryController extends BasePublicController
 {
     protected $guard;
-    public function __construct(Response $response,Guard $guard,UserRepository $user,ContentRepository $content,CategoryRepository $category)
+    public function __construct(Response $response,Guard $guard,UserRepository $user,ContentRepository $content,CategoryRepository $category,ContentLikeStoryRepository $likestory)
     {
        parent::__construct();
        $this->response = $response;
@@ -30,6 +33,7 @@ class StoryController extends BasePublicController
        $this->user = $user;
        $this->content = $content;
        $this->category = $category;
+       $this->likestory=$likestory;
        //$this->middleware('auth:api');
       // $this->middleware('oauth');
     }
@@ -37,6 +41,8 @@ class StoryController extends BasePublicController
       $validator = Validator::make($request->all(), [
           'category_id' => 'required'
       ]);
+       
+      $user_id=$request->user_id;
       if ($validator->fails()) {
           $errors = $validator->errors();
           foreach ($errors->all() as $message) {
@@ -47,10 +53,28 @@ class StoryController extends BasePublicController
       }else{
 
             $dataset = $this->content->filter('category_id' , $request->category_id);
-
+            foreach ($dataset as $key => $value) {             
+               $values=json_decode($value,true);
+               $value->islike=$this->checkLikeorNot($value,$user_id);
+               
+            }
+            
+            
+         
             return $dataset;
 
         }
+      }
+      public function checkLikeorNot($data =array(),$user_id)
+      {  
+        $likeData=DB::table('content__contentlikestories as cc')
+                      ->where('cc.content_id','=',$data['id'])
+                      ->where('cc.user_id','=',$user_id)
+                      ->get();
+                       if(count($likeData))
+                         return 1;
+                       else return 0;         
+
       }
      public function homepage(Request $request,Client $http){
             
@@ -68,6 +92,47 @@ class StoryController extends BasePublicController
             
 
             return $dataresponse;
+
+        }
+
+        public function story_like(Request $request)
+        { 
+               $user_id=$request->user_id;  
+               $content_id=$user_id;       
+               $data=DB::table('content__contentlikestories')
+                          ->where('content_id','=',$content_id)
+                           ->where('user_id','=',$user_id)
+                           ->get();
+
+               if(sizeof($data)>0)
+               { 
+                $data=DB::table('content__contentlikestories')
+                    ->where('content_id','=',$content_id)
+                    ->where('user_id','=',$user_id)
+                    ->delete();
+
+               }
+              else {
+                       
+                $abc['user_id']=$content_id;
+                $abc['content_id']=$content_id;
+                $data=$this->likestory->create($abc); 
+              }
+               
+          return response($data);
+        }
+        public function getAllLikeStory(Request $request)
+        {     Log::info($request->user_id);        
+          $dataset=DB::table('content__contents as cc')
+                  ->join('content__contentlikestories as ccl', 'cc.id','=','ccl.content_id')
+                   ->select('cc.*' )
+                   ->where('ccl.user_id','=',$request->user_id)                        
+                   ->paginate(12);
+                   
+
+
+       
+           return response($dataset);
 
         }
 }
