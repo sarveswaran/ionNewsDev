d0d974a3e79233ba8530a0e9df20129fe4c09b28
pradeep0492 <pradeep.gupta@mantralabsglobal.com>
2017-06-27 23:37:37 +0530

    change error style

--- Themes/Adminlte/views/errors/404.blade.php
+++ Themes/Adminlte/views/errors/404.blade.php
@@ -15,7 +15,9 @@
         <h2 class="headline text-yellow" style="line-height: 0.6; margin-top: 0;"> 404</h2>
         <div class="error-content">
             <h3><i class="fa fa-warning text-yellow"></i> {{ trans('core::core.error 404 title') }}</h3>
-            <p>{!! trans('core::core.error 404 description') !!}</p>
+            <!-- <p>{!! trans('core::core.error 404 description') !!}</p> -->
+             <p>Please got to the Home Page and 
+            <a href="{{ URL::to('/backend/content/contents') }}"> Try again</a></p>
         </div>
         <!-- /.error-content -->
     </div>
